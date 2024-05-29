adef0bf7af392932c15f7207c14ac3467b25452d
Birendra Ale Magar <44447991+alebirendra@users.noreply.github.com>
2024-05-29 00:20:26 -0400

    Update spline_fit.py

--- best_fit/spline_fit.py
+++ best_fit/spline_fit.py
@@ -1,15 +1,16 @@
 from scipy.interpolate import UnivariateSpline
+data = pd.read_csv('nc.dat', sep='\s+', names=["time_step", "nc" ])
 
 # Spline fitting
-spline = UnivariateSpline(data['x'], data['y'])
-spline.set_smoothing_factor(1)
+spline = UnivariateSpline(data['time_step'], data['nc'])
+spline.set_smoothing_factor(10)
 
 # Plotting
 plt.figure(figsize=(10, 6))
-plt.plot(data['x'], data['y'], label='Original Data', alpha=0.5)
-plt.plot(data['x'], spline(data['x']), label='Spline Fit', color='purple')
+plt.plot(data['time_step'], data['nc'], label='Original Data', alpha=0.5)
+plt.plot(data['time_step'], spline(data['time_step']), label='Spline Fit', color='purple')
 plt.legend()
-plt.xlabel('x')
-plt.ylabel('y')
+plt.xlabel('time_step')
+plt.ylabel('nc')
 plt.title('Spline Fitting')
 plt.show()
