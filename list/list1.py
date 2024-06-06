ae19892d0500f737196aef5f2f08b67ff9a151bc
Birendra Ale Magar <44447991+alebirendra@users.noreply.github.com>
2024-06-06 09:51:37 -0400

    Update list1.py

--- list/list1.py
+++ list/list1.py
@@ -1,12 +1,15 @@
-from itertools import product
+from itertools import combinations_with_replacement
 
 # Given lists
 list1 = ['A', 'A', 'A', 'A', 'A', 'A', 'A', 'A']
 list2 = ['B', 'B', 'B', 'B', 'B', 'B', 'B']
 list3 = ['C', 'C', 'C', 'C', 'C', 'C', 'C']
 
-# Generate all possible combinations of elements from the lists
-all_combinations = product(list1, list2, list3, repeat=1)
+# Concatenate the lists
+all_elements = list1 + list2 + list3
+
+# Generate all possible combinations of 12 elements
+all_combinations = combinations_with_replacement(all_elements, 12)
 
 # Create a set to store unique combinations
 unique_combinations = set()
@@ -16,8 +19,8 @@ for combination in all_combinations:
     unique_combinations.add(tuple(sorted(combination)))
 
 # Convert the set of unique combinations to a list of lists
-example_list = [list(combination) for combination in unique_combinations]
+list_of_lists = [list(combination) for combination in unique_combinations]
 
 # Print the list of lists
-for inner_list in example_list:
+for inner_list in list_of_lists:
     print(inner_list)
