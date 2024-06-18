b838bade6ead596b79fb55c76795ed92af3d2ce6
Birendra Ale Magar <44447991+alebirendra@users.noreply.github.com>
2024-06-18 11:10:36 -0400

    Update best_function.py

--- best_fit/best_function.py
+++ best_fit/best_function.py
@@ -3,11 +3,10 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
 # Sample data points
-lst = [5624.86, 7164.33, 7389.81, 7795.28, 9573.50 ]
-ek = [5, 10, 15, 20, 25]
-
-x = np.array(ek)
-y = np.array(lst)
+lst1 = [5810.78, 6421.90, 7097.30, 7843.73, 8668.66]
+lsek = [5, 10, 15, 20, 25]
+x = np.array(lsek)
+y = np.array(lst1)
 
 # Define candidate models
 def linear_model(x, a, b):
@@ -19,15 +18,23 @@ def quadratic_model(x, a, b, c):
 def exponential_model(x, a, b):
     return a * np.exp(b * x)
 
+def power_law(x, a, b):
+    return a * np.power(x, b)
+
+# Fit the power law function to the data
+params, covariance = curve_fit(power_law, x, y)
+
 # Fit candidate models to the data
 popt_linear, _ = curve_fit(linear_model, x, y)
 popt_quadratic, _ = curve_fit(quadratic_model, x, y)
 popt_exponential, _ = curve_fit(exponential_model, x, y)
+popt_power, _ = curve_fit(power_law, x, y)
 
 # equation
 linear_equation = f'y = {popt_linear[0]:.2f} x + {popt_linear[1]:.2f} '
-quad_equation = f'y = {popt_quadratic[0]:.2f} x**2 + {popt_quadratic[1]:.2f} + {popt_quadratic[2]:.2f}'
+quad_equation = f'y = {popt_quadratic[0]:.2f} x**2 + {popt_quadratic[1]:.2f}*x + {popt_quadratic[2]:.2f}'
 exp_equation = f'y = {popt_exponential[0]:.2f} exp({popt_exponential[1]:.2f}*x) '
+power_equation = f'y = {popt_power[0]:.2f} * x^({popt_power[1]:.2f}) '
 
 
 # Calculate goodness of fit measures (R-squared)
@@ -40,13 +47,17 @@ def r_squared(y, y_pred):
 y_linear = linear_model(x, *popt_linear)
 y_quadratic = quadratic_model(x, *popt_quadratic)
 y_exponential = exponential_model(x, *popt_exponential)
+y_power = exponential_model(x, *popt_power)
 
 r_squared_linear = r_squared(y, y_linear)
 r_squared_quadratic = r_squared(y, y_quadratic)
 r_squared_exponential = r_squared(y, y_exponential)
+r_squared_power = r_squared(y, y_power)
 
 # Select the best model based on R-squared
-best_model = max([(r_squared_linear, 'Linear'), (r_squared_quadratic, 'Quadratic'), (r_squared_exponential, 'Exponential')], key=lambda x: x[0])
+best_model = max([(r_squared_linear, 'Linear'), (r_squared_quadratic, 'Quadratic'), (r_squared_exponential, 'Exponential'),
+                 (r_squared_power, 'Power')], key=lambda x: x[0])
+
 print("Best-fit model:", best_model[1])
 
 # Plot the scatter plot
@@ -59,10 +70,13 @@ if best_model[1] == 'Linear':
 elif best_model[1] == 'Quadratic':
     print("Equation of best-fit curve:", quad_equation)
     plt.plot(x, quadratic_model(x, *popt_quadratic), color='red', label='Best-fit curve (Quadratic)')
-else:
+elif best_model[1] == 'Exponential':
     print("Equation of best-fit curve:", exp_equation)
     plt.plot(x, exponential_model(x, *popt_exponential), color='red', label='Best-fit curve (Exponential)')
-
+else:
+    print("Equation of best-fit curve:", power_equation)
+    plt.plot(x, power_law(x, *popt_power), color='red', label='Best-fit curve (Power)')
+    
 plt.xlabel('X')
 plt.ylabel('Y')
 plt.title('Scatter Plot with Best-fit Curve')
